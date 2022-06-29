43cd38c51a7c6e27455a0108195a9ef745d6003d
Reeve Schragger <80379457+rschragger@users.noreply.github.com>
2022-06-30 02:28:55 +1000

    Save location data to local

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,28 +16,41 @@ var useUnits = 'metric'; // allow choose 'imperial'
 var exclusions = 'minutely' // allow for 'current,minutely,hourly,daily,alerts'
 var fullCurrentWeather = {};
 
+//Search 
+var locationsList = [{ name: null }];
+var searchButton = document.getElementById('searchBtn');
+var searchTerm = '';
+searchButton.addEventListener('click', function () {
+    //event.preventDefault();
+    var searchInput = document.getElementById('cityInput');
+    searchTerm = searchInput.value;
+    searchInput.value = '';
+    locationAPI(searchTerm);
+});
+
 //Error modal
-var errorModal = document.getElementById("errorModal");
-var closeSpan = document.getElementsByClassName("closeSpan")[0];
-//Error modal functions
-// Open error modal
-function showErrorModal(message) {
-    var errorPElement = document.getElementById('errorMessage');
-    errorPElement.textContent = message;
-    errorModal.style.display = "block";
-}
-//close error modal
-// When the user clicks on <span> (x), close the modal
-closeSpan.onclick = function () {
-    errorModal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == errorModal) {
+{
+    var errorModal = document.getElementById("errorModal");
+    var closeSpan = document.getElementsByClassName("closeSpan")[0];
+    //Error modal functions
+    // Open error modal
+    function showErrorModal(message) {
+        var errorPElement = document.getElementById('errorMessage');
+        errorPElement.textContent = message;
+        errorModal.style.display = "block";
+    }
+    //close error modal
+    // When the user clicks on <span> (x), close the modal
+    closeSpan.onclick = function () {
         errorModal.style.display = "none";
     }
-}
-
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == errorModal) {
+            errorModal.style.display = "none";
+        }
+    }
+} //End errorModal
 
 // Functions ---------------------------------------------------------
 function locationAPI(searchTerm) {
@@ -48,7 +61,7 @@ function locationAPI(searchTerm) {
     fetch(requestUrl)
         .then(function (response) {
             if (response.status != 200) {
-                alert('Error: ' + response.status)
+                showErrorModal('Error: ' + response.status)
             }
             console.log(response)
             return response.json();
@@ -60,7 +73,7 @@ function locationAPI(searchTerm) {
             console.log(data);
             // testingVal=data;
             if (data.length < 1) {
-                alert('Nothing found');
+                showErrorModal('No result was found for that search term, please try again');
             } else {
                 locationData = {
                     'name': data[0].name,
@@ -69,20 +82,53 @@ function locationAPI(searchTerm) {
                     'lat': data[0].lat,
                     'lon': data[0].lon,
                 };
+                addLocationToLocal(JSON.stringify(locationData));
                 weatherAPI(locationData.lat, locationData.lon, exclusions, apiKey);
             }
         });
+}
+
+function addLocationToLocal(locData) {
+    locData = JSON.parse(locData);
+    if (localStorage.getItem('locationsList') == undefined) {
+        localStorage.setItem('locationsList', JSON.stringify(locationsList))
+            ;
+    }
+    locationsList = JSON.parse(localStorage.getItem('locationsList'));
+    var locListLength = locationsList.length;
+    if (!locListLength) { locListLength = [locData] }
+    //replace if existing
+    var thisName = locData.name;
+    var thisCountry = locData.country;
+
+
+    for (let index in locationsList) {
+        if (locationsList[index].name == thisName && locationsList[index].country == thisCountry) {
+            //delete this object as it will be added below
+
+            locListLength = index; //this sets up a replace at same spot
+            // locationsList.splice(locListLength, 1);
+        }
+
+
+    };
+    locationsList.splice(locListLength, 1, locData);
+    if(locationsList[0].name==null){locationsList.splice(0,1)};
+    console.log('loc list:' + JSON.stringify(locationsList))
+    localStorage.setItem('locationsList', JSON.stringify(locationsList));
+    locationsList={};
+
 }
 
 function weatherAPI(lat, lon, exclude, apiKey) {
     //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
     var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=' + exclude + '&units=' + useUnits + '&appid=' + apiKey;
-    window.locationData = {};
+    // window.locationData = {};
     fetch(requestUrl)
         .then(function (response) {
             if (response.status != 200) {
-                alert('Error: ' + response.status)
+                showErrorModal('Error: ' + response.status)
             }
             console.log(response)
             return response.json();
@@ -98,9 +144,9 @@ function weatherAPI(lat, lon, exclude, apiKey) {
 function init() {
     //showErrorModal('test message is written here')
 
-    console.log('final result:' + locationAPI('perth'));
+    //console.log('final result:' + locationAPI('perth'));
 
-    console.log(locationData);
+    //console.log(fullCurrentWeather);
 
 }
 

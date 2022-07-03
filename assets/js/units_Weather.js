425f28419e367be49cb783078d9baef074dee4a3
Reeve Schragger <80379457+rschragger@users.noreply.github.com>
2022-07-03 16:31:17 +1000

    Units data sorted

--- assets/js/units_Weather.js
+++ assets/js/units_Weather.js
@@ -1,334 +1,263 @@
 function init1() {
    localStorage.setItem("unitsData", unitsDataToStore);
 }
-var unitsDataToStore = JSON.stringify([
-   
+var unitsDataToStore = JSON.stringify(
    {
-      "id":{
-         "Parameter":"id",
-         "Description":"City identification",
-         "Standard":"-",
-         "Metric":"-",
-         "Imperial":"-"
+      "obj":{
+         "id":{
+            "parameter":"id",
+            "description":"City identification",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "dt":{
+            "parameter":"dt",
+            "description":"Data receiving time",
+            "standard":"unix, UTC",
+            "metric":"unix, UTC",
+            "imperial":"unix, UTC"
+         },
+         "name":{
+            "parameter":"name",
+            "description":"City name",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "coord":{
+            "parameter":"coord",
+            "description":"",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "lat":{
+            "parameter":"lat",
+            "description":"City geo location, latitude",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "lon":{
+            "parameter":"lon",
+            "description":"City geo location, longitude",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "sys":{
+            "parameter":"sys",
+            "description":"",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "message":{
+            "parameter":"message",
+            "description":"System parameter, do not use it",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "country":{
+            "parameter":"country",
+            "description":"Country code (GB, JP etc.)",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "sunrise":{
+            "parameter":"sunrise",
+            "description":"Sunrise time",
+            "standard":"unix, UTC",
+            "metric":"unix, UTC",
+            "imperial":"unix, UTC"
+         },
+         "sunset":{
+            "parameter":"sunset",
+            "description":"Sunset time",
+            "standard":"unix, UTC",
+            "metric":"unix, UTC",
+            "imperial":"unix, UTC"
+         },
+         "main":{
+            "parameter":"main",
+            "description":"",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "temp":{
+            "parameter":"temp",
+            "description":"Temperature",
+            "standard":"Kelvin",
+            "metric":"Celsius",
+            "imperial":"Fahrenheit"
+         },
+         "humidity":{
+            "parameter":"humidity",
+            "description":"Humidity",
+            "standard":"%",
+            "metric":"%",
+            "imperial":"%"
+         },
+         "temp_min":{
+            "parameter":"temp_min",
+            "description":"Minimum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally)",
+            "standard":"Kelvin",
+            "metric":"Celsius",
+            "imperial":"Fahrenheit"
+         },
+         "temp_max":{
+            "parameter":"temp_max",
+            "description":"Maximum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally)",
+            "standard":"Kelvin",
+            "metric":"Celsius",
+            "imperial":"Fahrenheit"
+         },
+         "pressure":{
+            "parameter":"pressure",
+            "description":"Atmospheric pressure (on the sea level, if there is no sea_level or grnd_level data)",
+            "standard":"hPa",
+            "metric":"hPa",
+            "imperial":"hPa"
+         },
+         "sea_level":{
+            "parameter":"sea_level",
+            "description":"Atmospheric pressure on the sea level",
+            "standard":"hPa",
+            "metric":"hPa",
+            "imperial":"hPa"
+         },
+         "grnd_level":{
+            "parameter":"grnd_level",
+            "description":"Atmospheric pressure on the ground level",
+            "standard":"hPa",
+            "metric":"hPa",
+            "imperial":"hPa"
+         },
+         "wind":{
+            "parameter":"wind",
+            "description":"",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "speed":{
+            "parameter":"speed",
+            "description":"Wind speed",
+            "standard":"meter/sec",
+            "metric":"meter/sec",
+            "imperial":"miles/hour"
+         },
+         "deg":{
+            "parameter":"deg",
+            "description":"Wind direction",
+            "standard":"degrees (meteorological)",
+            "metric":"degrees (meteorological)",
+            "imperial":"degrees (meteorological)"
+         },
+         "gust":{
+            "parameter":"gust",
+            "description":"Wind gust",
+            "standard":"meter/sec",
+            "metric":"meter/sec",
+            "imperial":"miles/hour"
+         },
+         "clouds":{
+            "parameter":"clouds",
+            "description":"",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "all":{
+            "parameter":"all",
+            "description":"Cloudiness",
+            "standard":"%",
+            "metric":"%",
+            "imperial":"%"
+         },
+         "weather(more info Weather condition codes)":{
+            "parameter":"weather (more info Weather condition codes)",
+            "description":"",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "id":{
+            "parameter":"id",
+            "description":"Weather condition id",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "main":{
+            "parameter":"main",
+            "description":"Group of weather parameters (Rain, Snow, Extreme etc.)",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "description":{
+            "parameter":"description",
+            "description":"Weather condition within the group",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "icon":{
+            "parameter":"icon",
+            "description":"Weather icon id",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "rain":{
+            "parameter":"rain",
+            "description":"",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "1h":{
+            "parameter":"1h",
+            "description":"Precipitation volume for last hour",
+            "standard":"mm",
+            "metric":"mm",
+            "imperial":"mm"
+         },
+         "3h":{
+            "parameter":"3h",
+            "description":"Precipitation volume for last 3 hours",
+            "standard":"mm",
+            "metric":"mm",
+            "imperial":"mm"
+         },
+         "snow":{
+            "parameter":"snow",
+            "description":"",
+            "standard":"",
+            "metric":"",
+            "imperial":""
+         },
+         "1h":{
+            "parameter":"1h",
+            "description":"Snow volume for last hour",
+            "standard":"mm",
+            "metric":"mm",
+            "imperial":"mm"
+         },
+         "3h":{
+            "parameter":"3h",
+            "description":"Snow volume for last 3 hours",
+            "standard":"mm",
+            "metric":"mm",
+            "imperial":"mm"
+         }
       }
-   },
-   {
-      "dt":{
-         "Parameter":"dt",
-         "Description":"Data receiving time",
-         "Standard":"unix, UTC",
-         "Metric":"unix, UTC",
-         "Imperial":"unix, UTC"
-      }
-   },
-   {
-      "name":{
-         "Parameter":"name",
-         "Description":"City name",
-         "Standard":"-",
-         "Metric":"-",
-         "Imperial":"-"
-      }
-   },
-   {
-      "coord":{
-         "Parameter":"coord",
-         "Description":"",
-         "Standard":"",
-         "Metric":"",
-         "Imperial":""
-      }
-   },
-   {
-      "lat":{
-         "Parameter":"lat",
-         "Description":"City geo location, latitude",
-         "Standard":"-",
-         "Metric":"-",
-         "Imperial":"-"
-      }
-   },
-   {
-      "lon":{
-         "Parameter":"lon",
-         "Description":"City geo location, longitude",
-         "Standard":"-",
-         "Metric":"-",
-         "Imperial":"-"
-      }
-   },
-   {
-      "sys":{
-         "Parameter":"sys",
-         "Description":"",
-         "Standard":"",
-         "Metric":"",
-         "Imperial":""
-      }
-   },
-   {
-      "message":{
-         "Parameter":"message",
-         "Description":"System parameter, do not use it",
-         "Standard":"-",
-         "Metric":"-",
-         "Imperial":"-"
-      }
-   },
-   {
-      "country":{
-         "Parameter":"country",
-         "Description":"Country code (GB, JP etc.)",
-         "Standard":"-",
-         "Metric":"-",
-         "Imperial":"-"
-      }
-   },
-   {
-      "sunrise":{
-         "Parameter":"sunrise",
-         "Description":"Sunrise time",
-         "Standard":"unix, UTC",
-         "Metric":"unix, UTC",
-         "Imperial":"unix, UTC"
-      }
-   },
-   {
-      "sunset":{
-         "Parameter":"sunset",
-         "Description":"Sunset time",
-         "Standard":"unix, UTC",
-         "Metric":"unix, UTC",
-         "Imperial":"unix, UTC"
-      }
-   },
-   {
-      "main":{
-         "Parameter":"main",
-         "Description":"",
-         "Standard":"",
-         "Metric":"",
-         "Imperial":""
-      }
-   },
-   {
-      "temp":{
-         "Parameter":"temp",
-         "Description":"Temperature",
-         "Standard":"Kelvin",
-         "Metric":"Celsius",
-         "Imperial":"Fahrenheit"
-      }
-   },
-   {
-      "humidity":{
-         "Parameter":"humidity",
-         "Description":"Humidity",
-         "Standard":"%",
-         "Metric":"%",
-         "Imperial":"%"
-      }
-   },
-   {
-      "temp_min":{
-         "Parameter":"temp_min",
-         "Description":"Minimum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally)",
-         "Standard":"Kelvin",
-         "Metric":"Celsius",
-         "Imperial":"Fahrenheit"
-      }
-   },
-   {
-      "temp_max":{
-         "Parameter":"temp_max",
-         "Description":"Maximum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally)",
-         "Standard":"Kelvin",
-         "Metric":"Celsius",
-         "Imperial":"Fahrenheit"
-      }
-   },
-   {
-      "pressure":{
-         "Parameter":"pressure",
-         "Description":"Atmospheric pressure (on the sea level, if there is no sea_level or grnd_level data)",
-         "Standard":"hPa",
-         "Metric":"hPa",
-         "Imperial":"hPa"
-      }
-   },
-   {
-      "sea_level":{
-         "Parameter":"sea_level",
-         "Description":"Atmospheric pressure on the sea level",
-         "Standard":"hPa",
-         "Metric":"hPa",
-         "Imperial":"hPa"
-      }
-   },
-   {
-      "grnd_level":{
-         "Parameter":"grnd_level",
-         "Description":"Atmospheric pressure on the ground level",
-         "Standard":"hPa",
-         "Metric":"hPa",
-         "Imperial":"hPa"
-      }
-   },
-   {
-      "wind":{
-         "Parameter":"wind",
-         "Description":"",
-         "Standard":"",
-         "Metric":"",
-         "Imperial":""
-      }
-   },
-   {
-      "speed":{
-         "Parameter":"speed",
-         "Description":"Wind speed",
-         "Standard":"meter/sec",
-         "Metric":"meter/sec",
-         "Imperial":"miles/hour"
-      }
-   },
-   {
-      "deg":{
-         "Parameter":"deg",
-         "Description":"Wind direction",
-         "Standard":"degrees (meteorological)",
-         "Metric":"degrees (meteorological)",
-         "Imperial":"degrees (meteorological)"
-      }
-   },
-   {
-      "gust":{
-         "Parameter":"gust",
-         "Description":"Wind gust",
-         "Standard":"meter/sec",
-         "Metric":"meter/sec",
-         "Imperial":"miles/hour"
-      }
-   },
-   {
-      "clouds":{
-         "Parameter":"clouds",
-         "Description":"",
-         "Standard":"",
-         "Metric":"",
-         "Imperial":""
-      }
-   },
-   {
-      "all":{
-         "Parameter":"all",
-         "Description":"Cloudiness",
-         "Standard":"%",
-         "Metric":"%",
-         "Imperial":"%"
-      }
-   },
-   {
-      "weather (more info Weather condition codes)":{
-         "Parameter":"weather (more info Weather condition codes)",
-         "Description":"",
-         "Standard":"",
-         "Metric":"",
-         "Imperial":""
-      }
-   },
-   {
-      "id":{
-         "Parameter":"id",
-         "Description":"Weather condition id",
-         "Standard":"-",
-         "Metric":"-",
-         "Imperial":"-"
-      }
-   },
-   {
-      "main":{
-         "Parameter":"main",
-         "Description":"Group of weather parameters (Rain, Snow, Extreme etc.)",
-         "Standard":"-",
-         "Metric":"-",
-         "Imperial":"-"
-      }
-   },
-   {
-      "description":{
-         "Parameter":"description",
-         "Description":"Weather condition within the group",
-         "Standard":"-",
-         "Metric":"-",
-         "Imperial":"-"
-      }
-   },
-   {
-      "icon":{
-         "Parameter":"icon",
-         "Description":"Weather icon id",
-         "Standard":"-",
-         "Metric":"-",
-         "Imperial":"-"
-      }
-   },
-   {
-      "rain":{
-         "Parameter":"rain",
-         "Description":"",
-         "Standard":"",
-         "Metric":"",
-         "Imperial":""
-      }
-   },
-   {
-      "1h":{
-         "Parameter":"1h",
-         "Description":"Precipitation volume for last hour",
-         "Standard":"mm",
-         "Metric":"mm",
-         "Imperial":"mm"
-      }
-   },
-   {
-      "3h":{
-         "Parameter":"3h",
-         "Description":"Precipitation volume for last 3 hours",
-         "Standard":"mm",
-         "Metric":"mm",
-         "Imperial":"mm"
-      }
-   },
-   {
-      "snow":{
-         "Parameter":"snow",
-         "Description":"",
-         "Standard":"",
-         "Metric":"",
-         "Imperial":""
-      }
-   },
-   {
-      "1h":{
-         "Parameter":"1h",
-         "Description":"Snow volume for last hour",
-         "Standard":"mm",
-         "Metric":"mm",
-         "Imperial":"mm"
-      }
-   },
-   {
-      "3h":{
-         "Parameter":"3h",
-         "Description":"Snow volume for last 3 hours",
-         "Standard":"mm",
-         "Metric":"mm",
-         "Imperial":"mm"
-      }
-   },
-   
-
-]);
+   }
+);
 
 init1()

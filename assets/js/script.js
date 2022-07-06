8c61ca7279d2360ba8a55a7a8f989c844a2d8b5a
Reeve Schragger <80379457+rschragger@users.noreply.github.com>
2022-07-06 16:47:23 +1000

    Updates- really improved the functions by re-writing from data and not from div

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,7 @@ var limitGeoNo = 1; //how many results for geolocation
 //Weather API variables
 var exclusions = 'minutely' // allow for 'current,minutely,hourly,daily,alerts'
 var fullCurrentWeather = {};
+var forecastWeatherLoc = {};
 
 //Search 
 var locationsList = [{ name: null }];
@@ -29,7 +30,7 @@ searchButton.addEventListener('click', function () {
     locationAPI(searchTerm);
 });
 
-//Error modal
+//window modal
 {
     var windowModal = document.getElementById("windowModal");
     var closeSpan = document.getElementsByClassName("closeSpan")[0];
@@ -257,87 +258,172 @@ function unitSuffix_set(type) {
         rootVar.style.setProperty('--length', `'km'`);
     }
 }
-function makeTimeByClass(thisID,thisDetailVal) {
-    if (thisID.className.includes('timeXhm')){
+function makeTimeByClass(thisID, thisDetailVal) {
+    if (thisID.className.includes('timeXhm')) {
         thisDetailVal = moment(thisDetailVal, 'X').format("hh:mma")
-    }else if(thisID.className.includes('timeXfull')){
-        thisDetailVal = moment(thisDetailVal,'X').format("dddd Do MMMM yyy hh:mma")
+    } else if (thisID.className.includes('timeXfull')) {
+        thisDetailVal = moment(thisDetailVal, 'X').format("dddd Do MMMM yyy hh:mma")
 
     }
-return thisDetailVal
+    return thisDetailVal
 }
 
 //functions to create the panels
 function createAllPanels() {
-    makeCurrentPanel()
-    makeForecastPanels()
+    writeLocation()
+    makeCurrentPanel();
+    makeForecastPanels();
 }
 
-function makeCurrentPanel() {
-    var currentWeatherLoc = fullCurrentWeather.current;
-    var currentDivs = document.querySelector('#todayWeather').children;
-    var replaceIdText = 'Today'
-
-    for (i = 0; i < currentDivs.length; i++) {
-        var detailNameId = currentDivs[i].id;
-        var detailName = detailNameId.replace(replaceIdText, '');
-        // var thisDetailVal = eval('currentWeatherLoc.' + detailName);
-        var thisDetailVal = currentWeatherLoc[detailName];
-        var thisID = document.getElementById(detailNameId);
-        if (thisID) {
-            //check for time
-            if (thisID.className.includes('timeX')) {
-                
-                thisDetailVal = makeTimeByClass(thisID,thisDetailVal)
-            }
-            thisID.textContent = thisDetailVal
-        }
+function writeLocation() {
+    var locNodes = document.querySelectorAll('.location');
+    for (i = 0; i < locNodes.length; i++) {
+        locNodes[i].textContent = locationData.name;
     }
+
+
 }
 
-function makeForecastPanels() {
-    var forecastWeatherLoc = fullCurrentWeather.daily;
+/* this function was too complex and didn't really work
 
-    for (d = 0; d < 5; d++) { //6 days=current day + 5 forecast days
-        var currentPanel = document.querySelector('#forecastPanel').children[d];
-        var currentDivs = currentPanel.children;
-        
-        var replaceIdText = 'Day' + (d+1)
-
+function makeCurrentPanelORIG() {
+    var replaceIdText = 'Today';
+    var currentWeatherLoc = fullCurrentWeather.current;
+    var mainDivs = document.querySelector('#day0').children;
+    for (md = 0; md < mainDivs.length; md++) {
+        var currentDivs = mainDivs[md].children;
         for (i = 0; i < currentDivs.length; i++) {
             var detailNameId = currentDivs[i].id;
             var detailName = detailNameId.replace(replaceIdText, '');
-            var thisDetailVal = forecastWeatherLoc[i][detailName] //could be an array { day: 10.11, min: 5.96, max: 15.32, night: 8.44, eve: 11.27 }
-         if(typeof thisDetailVal === 'object'){
-var childDivs = currentDivs[i].children
-for (cd = 0 ; cd < childDivs.length;cd++){
-    var ChildDetailNameId = childDivs[cd].id;
-    var ChildDetailName = ChildDetailNameId.replace(replaceIdText, '');
-    var ChildDetailVal = thisDetailVal[ChildDetailName];
-    var thisID = document.getElementById(ChildDetailNameId);
-    if (thisID) {
-        //check for time
-        if (thisID.className.includes('timeX')) {
-            
-            ChildDetailVal = makeTimeByClass(thisID,ChildDetailVal)
-        }
-        thisID.textContent = ChildDetailVal
-    }
-}
-         }else{
+            // var thisDetailVal = eval('currentWeatherLoc.' + detailName);
+
+
+
+            var thisDetailVal = currentWeatherLoc[detailName];
             var thisID = document.getElementById(detailNameId);
             if (thisID) {
                 //check for time
                 if (thisID.className.includes('timeX')) {
-                    
-                    thisDetailVal = makeTimeByClass(thisID,thisDetailVal)
+
+                    thisDetailVal = makeTimeByClass(thisID, thisDetailVal)
                 }
                 thisID.textContent = thisDetailVal
             }
         }
-    }}
+    }
+}*/
+
+/* this function was too complex and didn't really work
+function makeForecastPanelsORIG() {
+    var forecastWeatherLoc = fullCurrentWeather.daily;
+
+    for (d = 0; d < 6; d++) { //6 days=current day + 5 forecast days
+        var currentPanel = document.querySelector('#forecastPanel').children[d];
+        var replaceIdText = 'Day' + d;
+        // var currentDivs = currentPanel.children;
+
+        var mainDivs = document.querySelector('#day' + d).children;
+        //Get the main div's sub divs and go through
+        for (md = 0; md < mainDivs.length; md++) {
+            var currentDivs = mainDivs[md].children;
+            //Get the sub divs and go through adding data
+            for (i = 0; i < currentDivs.length; i++) {
+                var detailNameId = currentDivs[i].id;
+                var detailName = detailNameId.replace(replaceIdText, '');
+                var thisDetailVal = '';
+                if (detailName == 'weather') { thisDetailVal = forecastWeatherLoc[i][detailName][0] }
+                else { thisDetailVal = forecastWeatherLoc[i][detailName] } //could be an array { day: 10.11, min: 5.96, max: 15.32, night: 8.44, eve: 11.27 }
+                if (typeof thisDetailVal === 'object') {
+                    var childDivs = currentDivs[i].children
+                    for (cd = 0; cd < childDivs.length; cd++) {
+                        var ChildDetailNameId = childDivs[cd].id;
+                        var ChildDetailName = ChildDetailNameId.replace(replaceIdText, '');
+                        var ChildDetailVal = thisDetailVal[ChildDetailName];
+                        var thisID = document.getElementById(ChildDetailNameId);
+                        if (thisID) {
+                            //Add data here for array objects (check for time format and add)
+                            if (thisID.className.includes('timeX')) {
+                                ChildDetailVal = makeTimeByClass(thisID, ChildDetailVal)
+                            }
+                            thisID.textContent = ChildDetailVal
+                        }
+                    }
+                } else {
+                    //Add data here (check for time format and add)
+                    var thisID = document.getElementById(detailNameId);
+                    if (thisID) { //only add if the div id exists
+                        //check for time
+                        if (thisID.className.includes('timeX')) {
+                            thisDetailVal = makeTimeByClass(thisID, thisDetailVal)
+                        }
+                        thisID.textContent = thisDetailVal
+                    }
+                }
+            }
+        }
+    }
+}*/
+
+function makeForecastPanels() {
+    forecastWeatherLoc = fullCurrentWeather.daily;
+    for (i = 0; i < forecastWeatherLoc.length; i++) {
+        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
+        //for (const property in object) { console.log(`${property}: ${object[property]}`);}
+        for (const detailName in forecastWeatherLoc[i]) {
+            //console.log(`${detailName}`);
+            var thisDetailVal = '';
+            if (detailName == 'weather') { thisDetailVal = forecastWeatherLoc[i][detailName][0] }
+            else { thisDetailVal = forecastWeatherLoc[i][detailName] } //could be an array { day: 10.11, min: 5.96, max: 15.32, night: 8.44, eve: 11.27 }
+            if (typeof thisDetailVal === 'object') {
+                for (const subDetailName in thisDetailVal) {
+                    var detailId = detailName + `-${subDetailName}Day` + i;//e.g 'pressureDay1'
+                    setDiv(detailId, thisDetailVal[subDetailName]);
+                }
+            } else {
+                var detailId = detailName + 'Day' + i;//e.g 'pressureDay1'
+                setDiv(detailId, thisDetailVal)//.subDetailName);
+                // console.log(detailId);
+            }
+        }
+    }
 }
 
+function makeCurrentPanel() {
+    forecastWeatherLoc = fullCurrentWeather.current;
+    // for (i = 0; i < forecastWeatherLoc.length; i++) {
+        for (const detailName in forecastWeatherLoc) {
+            var thisDetailVal = '';
+            if (detailName == 'weather') { thisDetailVal = forecastWeatherLoc[detailName][0] }
+            else { thisDetailVal = forecastWeatherLoc[detailName] } //could be an array { day: 10.11, min: 5.96, max: 15.32, night: 8.44, eve: 11.27 }
+            if (typeof thisDetailVal === 'object') {
+                for (const subDetailName in thisDetailVal) {
+                    var detailId = detailName + `-${subDetailName}Today` //+ i;//e.g 'pressureToday'
+                    setDiv(detailId, thisDetailVal[subDetailName]);
+                }
+            } else {
+                var detailId = detailName + 'Today' //+ i;//e.g 'pressureToday'
+                setDiv(detailId, thisDetailVal)//.subDetailName);
+                // console.log(detailId);
+            }
+        }
+    // }
+}
+
+//<img src="https://openweathermap.org/img/wn/02n@4x.png" alt="example icon" >
+
+function setDiv(divId, divValue) {
+    var thisDiv = document.getElementById(divId)
+    if (thisDiv != null) {
+        //Check for time format and add
+        if (thisDiv.className.includes('timeX')) {
+            divValue = makeTimeByClass(thisDiv, divValue)
+        }
+        if (thisDiv.className.includes('iconImg')) {
+            divValue = `<img src="https://openweathermap.org/img/wn/${divValue}.png" alt="Icon for weather conditions" >`
+        }
+        thisDiv.innerHTML = divValue;
+    }
+}
 
 //init ---------------------------------------
 function init() {
